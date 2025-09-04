e40b4ef1531daa110adfde55abd1e3c3dfe54eda
Akshansh Patel <akshanshpatel003@gmail.com>
2025-09-04 22:09:33 +0530

    navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,20 @@
+import about from './about.png';
+import bin from './bin.png';
+import cart from './cart.png';
+import logo from './logo.png';
+import search from './search.png'
+import profile from './profile.png'
+import menu from './menu.png'
+import dropleft from './dropleft.png'
+
+
+export const assets={
+    about,
+    bin,
+    profile,
+    dropleft,
+    cart,
+    logo,
+    search,
+    menu
+}
