424a3dd6d964b9be3d16a5fe45dfec2bf4e22b9b
Akshansh Patel <akshanshpatel003@gmail.com>
2025-09-08 04:21:56 +0530

    added product funct

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,6 +6,9 @@ import search from './search.png'
 import profile from './profile.png'
 import menu from './menu.png'
 import dropleft from './dropleft.png'
+import billie from './billie.png'
+import dummy1 from './dummy1.png'
+import dummy2 from './dummy2.png'
 
 
 export const assets={
@@ -13,8 +16,37 @@ export const assets={
     bin,
     profile,
     dropleft,
+    billie,
     cart,
     logo,
     search,
-    menu
+    menu,
+    dummy1,
+    dummy2
 }
+
+export const products=[
+    {
+        _id:"aaaa",
+        name:"Billie photo_1",
+        description:"Dummy photograph 1",
+        price:100,
+        image:[dummy1],
+        category:"Women",
+        subCategory:"Topwear",
+        sizes:["S","M","L"],
+        date:1716634345448,
+        bestseller:true
+    },{
+        _id:"aaaa",
+        name:"Billie photo_2",
+        description:"Dummy photograph 2",
+        price:100,
+        image:[dummy2],
+        category:"Men",
+        subCategory:"Topwear",
+        sizes:["S","M","L"],
+        date:1716634345449,
+        bestseller:true
+    }
+]
